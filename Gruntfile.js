644a5a3f64b1c9f448011ba1f7d0f930608d3a02
Orde Saunders <orde@decadecity.net>
2014-05-20 18:33:39 +0100

    Now building talks via Reveal.js
    Reveal.js is now a submodule.

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    assemble: {
+      options: {
+        assets: "_src/assets",
+        data:   "_src/data/*.json"
+      },
+      project: {
+        options: {
+          layoutdir: '_src/views/layouts',
+          layout: 'master.hbs',
+          partials: '_src/views/partials/**/*.hbs'
+        },
+        files: [
+          {
+            cwd: '_src/views/presentations',
+            expand: true,
+            src: '**/*.hbs',
+            dest: 'public_html'
+          }
+        ]
+      }
+    },
+
+    less: {
+      themes: {
+        options: {},
+        files: {
+          "public_html/css/theme/decadecity.css": "_src/less/decadecity.less"
+        }
+      }
+    },
+
+    copy: {
+      reveal: {
+        files: [
+          {expand: true, flatten: true, cwd: 'lib/reveal.js/css/', src: ['*.css'], dest: 'public_html/css/'},
+          {expand: true, flatten: true, cwd: 'lib/reveal.js/js/', src: ['*.js'], dest: 'public_html/js/'},
+          {expand: true, flatten: false, cwd: 'lib/reveal.js/plugin/', src: ['**'], dest: 'public_html/plugin/'},
+          {expand: true, flatten: false, cwd: 'lib/reveal.js/lib/', src: ['**'], dest: 'public_html/lib/'},
+        ]
+      }
+    },
+
+    watch: {
+      templates: {
+        files: ['_src/views/**/*.hbs', '_src/views/**/*.html'],
+        tasks: ['assemble'],
+        options: {
+        }
+      },
+      less: {
+        files: ['_src/less/**/*.less'],
+        tasks: ['less'],
+        options: {
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('assemble' );
+  grunt.registerTask('default', ['less', 'assemble', 'copy', 'watch' ]);
+
+};
